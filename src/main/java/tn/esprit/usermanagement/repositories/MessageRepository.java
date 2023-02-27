f753b77ca7c0750d7252494b86e30174b28ca649
hela <hela.talbi@esprit.tn>
2023-02-27 21:28:36 +0100

    second commit

--- src/main/java/tn/esprit/usermanagement/repositories/MessageRepository.java
+++ src/main/java/tn/esprit/usermanagement/repositories/MessageRepository.java
@@ -1,7 +1,7 @@
 package tn.esprit.usermanagement.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.messaging.Message;
+import tn.esprit.usermanagement.entities.Message;
 
 public interface MessageRepository extends JpaRepository<Message, Integer>{
 
