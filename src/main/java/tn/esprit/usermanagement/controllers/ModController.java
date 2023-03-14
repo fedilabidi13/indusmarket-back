cdfe1a15d096e38b8a89039e99f9919534c9ba01
Oussama_Lachiheb <oussama.lachiheb@esprit.tn>
2023-03-14 08:13:16 +0100

    Final Task..

--- src/main/java/tn/esprit/usermanagement/controllers/ModController.java
+++ src/main/java/tn/esprit/usermanagement/controllers/ModController.java
@@ -2,24 +2,18 @@ package tn.esprit.usermanagement.controllers;
 
 import com.maxmind.geoip2.exception.GeoIp2Exception;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.usermanagement.dto.AuthenticationRequest;
 import tn.esprit.usermanagement.dto.AuthenticationResponse;
-import tn.esprit.usermanagement.entities.Claims;
 import tn.esprit.usermanagement.entities.User;
-import tn.esprit.usermanagement.enumerations.StatusClaims;
-import tn.esprit.usermanagement.enumerations.TypeClaim;
 import tn.esprit.usermanagement.repositories.UserRepo;
 import tn.esprit.usermanagement.services.AdminService;
 import tn.esprit.usermanagement.servicesImpl.AuthenticationService;
-import tn.esprit.usermanagement.servicesImpl.ClaimsServiceImpl;
 
 import java.io.IOException;
-import java.util.List;
 
 @Controller
 @AllArgsConstructor
@@ -28,14 +22,15 @@ public class ModController {
     private AuthenticationService authenticationService;
     private PasswordEncoder passwordEncoder;
     private AdminService adminService;
-   private ClaimsServiceImpl claimsService;
 
-    @GetMapping("/mod")
+
+    @GetMapping("/api/v1/auth/mod")
     public String showForm(Model model) {
         model.addAttribute("formData", new AuthenticationRequest("",""));
         return "form";
     }
-    @PostMapping("/mod/submit")
+
+    @PostMapping("/api/v1/auth/mod/submit")
     public String submitForm(@ModelAttribute("formData") AuthenticationRequest formData, Model model) throws IOException, GeoIp2Exception {
         if (userRepo.findByEmail2(formData.getEmail()).getFirtAttempt())
         {
@@ -48,7 +43,8 @@ public class ModController {
             return "ok";
         }
     }
-    @PostMapping("/mod/confirmPassword")
+
+    @PostMapping("/api/v1/auth/mod/confirmPassword")
     public String passForm(@ModelAttribute("formData") AuthenticationRequest formData2) {
         String password = formData2.getPassword();
         String email = formData2.getEmail();
@@ -58,10 +54,12 @@ public class ModController {
         userRepo.save(user);
         return "ok2";
     }
-    @PostMapping("/mod/approveProduct")
+
+    @PostMapping("/api/v1/auth/mod/approveProduct")
     public String approveProd(@RequestParam Integer id)
     {
         return adminService.approveProduct(id);
     }
+
 
 }
